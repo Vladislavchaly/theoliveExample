f7f31dbf72a898fe229ea14f364c6f8f142dd299
chaly <vladislav.chalyi@playwing.net>
2023-12-07 15:50:22 +0200

    make config changes

--- socket/server.ts
+++ socket/server.ts
@@ -1,45 +1,59 @@
-const { createServer } = require('http')
-const { Server } = require('socket.io')
+const {createServer} = require('http')
+const {Server} = require('socket.io')
 let spawn = require('child_process').spawn;
 
 const httpServer = createServer()
 const io = new Server(httpServer, {
-  cors: {
-    origin: '*',
-  },
+    cors: {
+        origin: '*',
+    },
 })
 
 const key = ''
 io.on('connection', (socket) => {
-  console.log(socket.connected)
-  socket.on('stream', (data) => {
-    const ffmpeg = spawn('ffmpeg', [
-      "-f",
-      "lavfi",
-      "-i",
-      "anullsrc",
-      "-i",
-      "-",
-      "-c:v",
-      "libx264",
-      "-preset",
-      "veryfast",
-      "-tune",
-      "zerolatency",
-      "-c:a",
-      "aac",
-      "-f",
-      "flv",
-      `rtmps://rtmp.europe-west.theo.live/live/${key}`
-    ]).on('error',function(m){
-      console.log(m)
-      console.error("FFMpeg not found in system cli; please install ffmpeg properly or make a softlink to ./!");
-      process.exit(-1);
-    });
 
-     ffmpeg.stdin.write(data, (c) => {
-       console.log(c)
-     })
-  })
+    socket.on('stream', (data) => {
+
+
+        const ffmpeg = spawn('ffmpeg', [
+            '-re',
+            '-f', 'lavfi',
+            '-i', 'testsrc=size=1280x720:rate=30',
+            '-f', 'lavfi',
+            '-i', 'anullsrc=channel_layout=stereo:sample_rate=44100',
+            '-pix_fmt', 'yuv420p',
+            '-c:v', 'libx264',
+            '-x264-params', 'keyint=60:scenecut=0',
+            '-tune', 'zerolatency',
+            '-preset', 'superfast',
+            '-c:a', 'aac',
+            '-f', 'flv',
+            `rtmps://rtmp.europe-west.theo.live/live/${key}`
+        ]).on('error', function (m) {
+            console.log(m)
+            console.error("FFMpeg not found in system cli; please install ffmpeg properly or make a softlink to ./!");
+            process.exit(-1);
+        });
+        ffmpeg.stdin.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+
+
+        ffmpeg.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        ffmpeg.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+
+        ffmpeg.on('error', (err) => {
+            console.error(`Error starting ffmpeg: ${err}`);
+        });
+
+        ffmpeg.stdin.write(data)
+
+
+    })
 })
 httpServer.listen(9999)
